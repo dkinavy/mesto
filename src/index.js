c675a0293bcfe24fc5c218f05de1eee36f71a991
dkinavy <dennis.kinyaev@gmail.com>
2020-12-01 13:43:11 +0300

    webpack added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 
-import {Card, initialCards} from '../components/Card.js';
-import {FormValidator} from '../components/FormValidator.js';
-import {Popup} from '../components/Popup.js';
-import {PopupWithForm} from '../components/PopupWithForm.js';
-import {PopupWithImage} from '../components/PopupWithImage.js';
-import {Section} from '../components/Section.js';
-import {UserInfo} from '../components/UserInfo.js';
+import {Card, initialCards} from './components/Card.js';
+import {FormValidator} from './components/FormValidator.js';
+import {Popup} from './components/Popup.js';
+import {PopupWithForm} from './components/PopupWithForm.js';
+import {PopupWithImage} from './components/PopupWithImage.js';
+import {Section} from './components/Section.js';
+import {UserInfo} from './components/UserInfo.js';
 
 const configs = ({
     formSelector: '.popup__form',
